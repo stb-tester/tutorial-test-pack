120ed83b037e3059c393b4f77de72e20d21b38b7
David Röthlisberger <david@stb-tester.com>
2019-10-16 16:18:19 +0100

    youtube.Search: Page object to read selection on YouTube search keyboard
    See https://stb-tester.com/blog/2019/10/16/keyboard-part-1

---
+++ tests/youtube/search/search.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+"""Copyright 2019 Stb-tester.com Ltd <support@stb-tester.com>"""
+
+import stbt
+
+
+class Search(stbt.FrameObject):
+    """The "Search" page of the YouTube app.
+
+    This is a *Page Object* because it inherits from `stbt.FrameObject`.
+    For more information about Page Objects see
+    <https://stb-tester.com/manual/object-repository>.
+    """
+
+    KEYBOARD_GRID = stbt.Grid(
+        region=stbt.Region(x=125, y=165, right=430, bottom=470),
+        data=[
+            "abcdef",
+            "ghijkl",
+            "mnopqr",
+            "stuvwx",
+            "yz1234",
+            "567890"])
+    BOTTOM_GRID = stbt.Grid(
+        region=stbt.Region(x=125, y=470, right=430, bottom=525),
+        data=[["SPACE", "DELETE", "CLEAR"]])
+
+    @property
+    def is_visible(self):
+        """Are we on YouTube's Search page?
+
+        Note: An instance of this class is truthy if ``is_visible`` returns
+        True, thanks to `stbt.FrameObject` magic.
+        """
+        return stbt.match("Search.png", frame=self._frame)
+
+    @property
+    def selection(self):
+        """The name of the selected button on the on-screen keyboard.
+
+        For example 'g' or 'CLEAR'.
+        """
+        m = stbt.match("selection.png", frame=self._frame,
+                       region=stbt.Region.bounding_box(
+                           self.KEYBOARD_GRID.region,
+                           self.BOTTOM_GRID.region))
+        for grid in [self.KEYBOARD_GRID, self.BOTTOM_GRID]:
+            try:
+                text = grid.get(region=m.region).data
+                return text
+            except IndexError:
+                pass
+        assert False, "Matched selection %r outside of known locations" % (m.region,)
